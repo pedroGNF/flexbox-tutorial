11cc1e72be39e44e7ede47739fa33f6d75ee4374
pedroGNF <pedrognfwork@gmail.com>
2023-01-24 23:33:48 -0300

    first commit

---
+++ display-flex/script.js
@@ -0,0 +1,16 @@
+/* Open the sidebar navigator*/
+function openNav() {
+  document.getElementById("mySidenav").style.width = "15.625rem";
+}
+/* close the sidebar navigator*/
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0rem";
+}
+/* Light Theme function */
+function lightTheme() {
+  document.body.style.backgroundColor = "lightblue";
+}
+/* Dark Theme function */
+function darkTheme() {
+  document.body.style.backgroundColor = "";
+}
---
+++ script.js
@@ -0,0 +1,20 @@
+/* Open the sidebar navigator*/
+function openNav() {
+  document.getElementById("mySidenav").style.width = "15.625rem";
+}
+/* close the sidebar navigator*/
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0rem";
+}
+/* Light Theme function */
+function lightTheme() {
+  document.body.style.backgroundColor = "lightblue";
+}
+/* Dark Theme function */
+function darkTheme() {
+  document.body.style.backgroundColor = "";
+}
+
+function cardBtn() {
+  document.getElementById("cardBtn").style.borderRadius = "50%";
+}
